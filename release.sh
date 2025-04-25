a6d47e1e3a438bc5031cf2cfda261816867840a4
痞子鱼 <siseboy@users.noreply.github.com>
2025-04-25 13:59:47 +0800

    fix

--- release.sh
+++ release.sh
@@ -107,19 +107,12 @@ function GenerateRules() {
     case ${software_name} in
         adguardhome)
             domestic_dns=(
+                "https://1.12.12.12/dns-query"
                 "h3://223.5.5.5/dns-query"
-		"https://1.12.12.12/dns-query"
-                # "https://dns.alidns.com:443/dns-query"
-                # "https://dns.ipv6dns.com:443/dns-query"
-                # "https://doh.360.cn:443/dns-query"
-                # "https://doh.pub:443/dns-query"
             )
             foreign_dns=(
-                "https://1.1.1.1/dns-query"
-                "https://101.101.101.101/dns-query"
-                # "https://8.8.8.8/dns-query"
-                # "https://dns.opendns.com:443/dns-query"
-                # "https://dns11.quad9.net:443/dns-query"
+                "h3://1.1.1.1/dns-query"
+                "https://8.8.8.8/dns-query"
             )
             function GenerateRulesHeader() {
                 echo -n "[/" >> "${file_path}"
